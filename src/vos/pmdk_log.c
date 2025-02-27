8d9b61f2c315130690efecfd679f55e1372134b4
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-02-27 18:23:35 +0100

    Tests: Let's try to mute NLT errors
    final test whatith excludeFile
    try to filter by category
    fix pmdk/.+
    suppress pmdk logs based on filename prefix
    try tu suppress PMDK warnings in VOS
    lets test the final solution
    optimize path in new way
    exclude set.c in analysis
    ignore errors in custom pipeline-lib
    ignore fatal error in nlttest validation
    remove dummy file set.c
    fix too long file names
    fix path to nlt-erros.json
    extend treshold
    exclude nlt_test from cat command
    restore original Jeninse file
    remove set.c to see error logs in details
    restore org (bad) path to set.c
    move NLT tests to Build stage
    print nlt logs before they are removed
    
    Cancel-prev-build: false
    Parallel-build: true
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu20-rpm: true
    Skip-build-el8-gcc-dev: true
    
    Skip-build-leap15-icc: true
    
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-bullseye: true
    
    Skip-func-test: true
    Skip-fault-injection-test: true
    Skip-func-test-vm-all: true
    Skip-func-test-hw: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
    
    Revert "Tests: Let's try to triger NLT errors"
    
    This reverts commit 877f40fca0253f79957c7b28ea6fd022be313fba.
    
    Revert "Tests: Let's try to triger NLT errors"
    
    This reverts commit 877f40fca0253f79957c7b28ea6fd022be313fba.
    
    Tests: Let's try to triger NLT errors
    
    ignore fatal error in nlttest validation
    remove dummy file set.c
    fix too long file names
    fix path to nlt-erros.json
    extend treshold
    exclude nlt_test from cat command
    restore original Jeninse file
    remove set.c to see error logs in details
    restore org (bad) path to set.c
    move NLT tests to Build stage
    print nlt logs before they are removed
    
    Cancel-prev-build: false
    Parallel-build: true
    Skip-build-el8-rpm: true
    Skip-build-el9-rpm: true
    Skip-build-leap15-rpm: true
    Skip-build-ubuntu20-rpm: true
    Skip-build-el8-gcc-dev: true
    
    Skip-build-leap15-icc: true
    
    Skip-unit-test: true
    Skip-unit-test-memcheck: true
    Skip-bullseye: true
    
    Skip-func-test: true
    Skip-fault-injection-test: true
    Skip-func-test-vm-all: true
    Skip-func-test-hw: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- src/vos/pmdk_log.c
+++ src/vos/pmdk_log.c
@@ -41,22 +41,23 @@ static void
 pmdk_log_function(enum pmemobj_log_level level, const char *file_name, unsigned line_no,
 		  const char *function_name, const char *message)
 {
-	/* normalize file path - remove leading "../" from the file_name */
+	/* normalize file path - remove leading "../" */
 	while ((*file_name == '.') && (*(file_name + 1) == '.') && (*(file_name + 2) == '/')) {
 		file_name += 3;
 	}
 
-#if 0
-/* add "pmdk/" prefix to all files names */
+/** Add "pmdk/" prefix to file name
+  * Prefix is needed to filtered out PMDK messages in NLT resultst analysis
+  * as it is implemented in https://github.com/daos-stack/pipeline-lib/pull/457
+  */
 #define PMDK_LOG_FUNCTION_MAX_FILENAME 255
-	char  file_name_buff[PMDK_LOG_FUNCTION_MAX_FILENAME] = "tg/";
+	char  file_name_buff[PMDK_LOG_FUNCTION_MAX_FILENAME] = "pmdk/";
 	char *local_file_name                                = file_name_buff + 5;
 	while ((local_file_name < file_name_buff + PMDK_LOG_FUNCTION_MAX_FILENAME - 1) &&
 	       (*file_name != '\0')) {
 		*(local_file_name++) = *(file_name++);
 	}
 	*local_file_name = '\0';
-#endif
 
 /*
  * There is a set of handy macros for each of the message priorities
@@ -78,7 +79,7 @@ pmdk_log_function(enum pmemobj_log_level level, const char *file_name, unsigned 
  * line number and the function name from the local variables.
  */
 #define PMDK_LOG_NOCHECK(mask, fmt, ...)                                                           \
-	d_log(mask, "%s:%d %s() " fmt, file_name, line_no, function_name, ##__VA_ARGS__)
+	d_log(mask, "%s:%d %s() " fmt, file_name_buff, line_no, function_name, ##__VA_ARGS__)
 
 	int *saved_mask = pmemobj_log_level_2_daos_log[level].saved_mask;
 	_D_DEBUG_W_SAVED_MASK(PMDK_LOG_NOCHECK, *saved_mask,
